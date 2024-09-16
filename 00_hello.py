1bc3d35feec22538993a4f879ba5e4c50ceb8781
caindra <mmirgal868@g.educaand.es>
2024-09-16 18:10:39 +0200

    file 00 uploaded

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Hola mundo
+print("Hola Python")
+# Hola mundo pero con ';' al final de la instrucción
+print("Hola Pyhon con punto y coma al final");
+print('Hola Python con comillas simples');
+
+"""
+Funciona de ambas maneras, pero la más común es la primera, 
+que es la que forma que se suele encontrar habitualmente,
+ya que Python entiende las intrucciones según cada línea de forma independiente 
+"""
+'''
+Esta es otra forma de hacer comentarios multilínea
+'''
+print('Tipos de datos: ')
+print(type(10))                     # Int
+print(type(10.5))                   # Float
+print(type(True))                   # Boleano
+print(type(1 + 3j))                 # Número complejo
+print(type('10'))                   # String
+print(type([10, 11, 12]))           # Array o Lista
+print(type({'name': 'John Doe'}))   # Diccionario
+print(type({10, 11, 12}))           # Set
+print(type((10, 11, 12)))           # Tupla
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Learning Python
 -----------------
 
-This repository includes the exercises I followed from @mouredev's tutorial to learning Python.
+This repository includes the exercises I followed from @mouredev's tutorial to learning Python. Most of the comments that can be found on the files will be in Spanish for easier references for my learning
